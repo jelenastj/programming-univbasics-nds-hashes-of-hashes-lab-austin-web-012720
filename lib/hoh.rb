5d869027fd8481929baf75b3c3d80e6d439bab07
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-04 18:57:31 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,13 +31,14 @@
 # }
 
 def naming_system
+  
   naming_system ={
     
     label: "Kingdom",
     sub_category: {
-    label: "Phylum",
-    sub_category: {      
-      label: "Class"
+      label: "Phylum",
+      sub_category: {      
+        label: "Class"
     }
   }
 
